814aa129094645435354612c4985f464443f2369
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-18 14:31:20 +0600

    customize client api from ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,            
             'description' => $this->detail,            
             'price' => $this->price,            
-            'stock' => $this->stock,            
-            'discount' => $this->discount            
+            'stock' => $this->stock > 0 ? ($this->stock) : "Out of stock",            
+            'discount' => $this->discount,
+            'discountPrice' => round((1-($this->discount/100))*$this->price,2),
+            'rating' => $this->reviews->count() > 0 ? (round($this->reviews->sum('star')/$this->reviews->count(),2)) : "No rating yet",
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]           
 
         ];
     }
