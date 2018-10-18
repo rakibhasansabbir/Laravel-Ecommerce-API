ce4d183870d90ac722cc874cb334e51d61988d6d
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-18 12:16:08 +0600

    create model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix' => 'products'],function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
+
+});
