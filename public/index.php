200adf9c13c6ed24f453f4c44efed3ff228e5c83
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-20 00:12:06 +0600

    Demo

--- public/index.php
+++ public/index.php
@@ -58,3 +58,8 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
