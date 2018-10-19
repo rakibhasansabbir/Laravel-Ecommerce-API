8b082d5bdc2d532f7ff9a274c852ba97c33937f3
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-19 23:04:50 +0600

    create review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Review;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
 
 class ReviewController extends Controller
@@ -35,9 +36,13 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ],201);
     }
 
     /**
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+
+    protected $fillable = [
+        'customer','review','star'
+    ];
+
     public function product(){
         return $this->belongsTo(Product::class);
     }
