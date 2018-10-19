a31cd969d628918a8b02276a924fd4b10cf7e812
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-20 00:17:30 +0600

    delete index.php added method

--- public/index.php
+++ public/index.php
@@ -59,7 +59,7 @@ $response->send();
 
 $kernel->terminate($request, $response);
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
+// $app->get('/cowsay', function() use($app) {
+//   $app['monolog']->addDebug('cowsay');
+//   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+// });
