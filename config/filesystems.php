64e4613f065f227f7d4452abe686b5208badfbd7
sabbirsr <rakibhasansabbir@yahoo.com>
2018-10-29 01:32:43 +0600

    change file system.php file

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'public'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,8 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path() . '/storage/images',
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
 
